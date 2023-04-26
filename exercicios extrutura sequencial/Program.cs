121c179a0bee6a281081bf7362e224ff68532199
vitor <vitor@DESKTOP-4OIHHQ9>
2023-04-26 16:04:45 -0300

    Add project files.

---
+++ exercicios extrutura sequencial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main()
+    {
+        double pi = 3.14159;
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double area;
+
+        area = pi * (raio * raio);
+
+        Console.WriteLine(area.ToString("F4", CultureInfo.InvariantCulture));
+    }
+}
